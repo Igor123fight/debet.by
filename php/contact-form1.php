8392569cb2984705d7a6d86b6a9ae72567707a35
Igor123fight <94051391+Igor123fight@users.noreply.github.com>
2022-04-29 14:23:00 +0300

    Update contact-form1.php

--- php/contact-form1.php
+++ php/contact-form1.php
@@ -1,52 +1,124 @@
-<?
-	$b24Url = "https://debetby.bitrix24.ru/";	// укажите URL своего Битрикс24
-	$b24UserID = "1";						// ID пользователя, от имени которого будем добавлять лид
-	$b24WebHook = "xv60sphb5cxdcuzz";		// код вебхука, который мы только что получили
-	
-	// формируем URL, на который будем отправлять запрос
-	$queryURL = "$b24Url/rest/$b24UserID/$b24WebHook/crm.lead.add.json";	
-	
-	// формируем параметры для создания лида	
-	$queryData = https_build_query(array(
-		"fields" => array(
-			"TITLE" => "Лид с нашего сайта",	// название лида
-			"NAME" => "Меган Фокс",				// имя ;)
-			'EMAIL' => Array(
-				"n0" => Array(
-					"VALUE" => str_replace(" ","",$_POST["email"]),
-					"VALUE_TYPE" => "WORK",
-				),
-			), // Рабочая эл. почта
-			//"PHONE" => array(	// телефон в Битрикс24 = массив, поэтому даже если передаем 1 номер, то передаем его в таком формате
-		//		"n0" => array(
-		//			"VALUE" =>  "+7 (123) 456-78-99",	// ненастоящий номер Меган Фокс
-		//			"VALUE_TYPE" => "MOBILE",			// тип номера = мобильн
-		"COMMENTS" => "Комментарий",
-			),
-		),
-	)
-		'params' => array("REGISTER_SONET_EVENT" => "Y")	// Y = произвести регистрацию события добавления лида в живой ленте. Дополнительно будет отправлено уведомление ответственному за лид.	
-	));
+<?php
 
-	// отправляем запрос в Б24 и обрабатываем ответ
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_SSL_VERIFYPEER => 0,
-		CURLOPT_POST => 1,
-		CURLOPT_HEADER => 0,
-		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_URL => $queryURL,
-		CURLOPT_POSTFIELDS => $queryData,
-	));
-	$result = curl_exec($curl);
-	curl_close($curl);
-	$result = json_decode($result,1); 
- 
-	// если произошла какая-то ошибка - выведем её
-	if(array_key_exists('error', $result))
-	{      
-		die("Ошибка при сохранении лида: ".$result['error_description']);
-	}
-	
-	echo "Лид добавлен, отличная работа :)";
-?>
+  $name 		    = isset($_POST['name']) ? $_POST['name'] : '';
+  $phone 		    = isset($_POST['phone']) ? $_POST['phone'] : '';
+  $description      = isset($_POST['description']) ? $_POST['description'] : '';
+  $email 			= isset($_POST['email']) ? $_POST['email'] : '';
+  $company          = isset($_POST['company']) ? $_POST['company'] : '';
+
+  $contact = array(
+    'NAME' => $name,
+    'PHONE' => $phone,
+    'DESCRIPTION' => $description,
+    'EMAIL' => $email,
+    'COMPANY' => $company,
+    'CONTACT_ID' => 0,
+    'COMPANY_ID' => 0,
+    'DEAL_ID' => 0,
+  );
+
+  $webhook_url = 'https://debetby.bitrix24.ru/rest/1/xv60sphb5cxdcuzz/'
+
+  $contact['COMPANY_ID'] = addCompany($contact);
+  $contact['CONTACT_ID'] = addContact($contact);
+  $contact['DEAL_ID'] = addDeal($contact);
+
+  if($contact['DESCRIPTION'] != '') addMessage($contact);
+
+  echo json_encode($contact['DEAL_ID'], JSON_UNESCAPED_UNICODE);
+
+  function sendDataToBitrix($method, $data) {
+    $queryUrl = webhook_url . $method ;
+    $queryData = http_build_query($data);
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_SSL_VERIFYPEER => 0,
+      CURLOPT_POST => 1,
+      CURLOPT_HEADER => 0,
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_URL => $queryUrl,
+      CURLOPT_POSTFIELDS => $queryData,
+    ));
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, 1);
+  }
+
+  function addDeal($contact) {
+    $dealData = sendDataToBitrix('crm.deal.add', [
+      'fields' => [
+        'TITLE' => 'Заявка с сайта',
+        'STAGE_ID' => 'NEW',
+        'CONTACT_ID' => $contact['CONTACT_ID'],
+      ], 'params' => [
+        'REGISTER_SONET_EVENT' => 'Y'
+      ]
+    ]);
+
+    return $dealData['result'];
+  }
+
+  function addContact($contact) {
+    $check = checkContact($contact);
+    if($check['total'] != 0) return $check['result'][0]['ID'];
+    $contactData = sendDataToBitrix('crm.contact.add', [
+      'fields' => [
+        'NAME' => $contact['NAME'],
+        'EMAIL' => [['VALUE' => $contact['EMAIL'], 'VALUE_TYPE' => 'WORK']],
+        'PHONE' => [['VALUE' => $contact['PHONE'], 'VALUE_TYPE' => 'WORK']],
+        'TYPE_ID' => 'CLIENT',
+        'COMPANY_ID' => $contact['COMPANY_ID'],
+      ], 'params' => [
+        'REGISTER_SONET_EVENT' => 'Y'
+      ]
+    ]);
+
+    return $contactData['result'];
+  }
+
+  function addCompany($contact) {
+    $check = checkCompany($contact);
+    if($check['total'] != 0) return $check['result'][0]['ID'];
+    $companyData = sendDataToBitrix('crm.company.add', [
+      'fields' => [
+        'TITLE' => $contact['COMPANY'],
+      ], 'params' => [
+        'REGISTER_SONET_EVENT' => 'Y'
+      ]
+    ]);
+    return $companyData['result'];
+  }
+
+  function addMessage($contact) {
+    $messageData = sendDataToBitrix('crm.livefeedmessage.add', [
+      'fields' => [
+        'MESSAGE' => $contact['DESCRIPTION'],
+        'POST_TITLE' => 'Сообщение с формы сайта',
+        'ENTITYTYPEID' => 2,
+        'ENTITYID' => $contact['DEAL_ID'],
+      ], 'params' => [
+        'REGISTER_SONET_EVENT' => 'Y'
+      ]
+    ]);
+    return $messageData['result'];
+  }
+
+  function checkCompany($contact){
+    $list = sendDataToBitrix('crm.company.list', [
+      'filter' => [ 'TITLE' =>  $contact['COMPANY']],
+      'select' => [ 'ID'],
+    ]);
+    return $list;
+  }
+
+  function checkContact($contact){
+    $list = sendDataToBitrix('crm.contact.list', [
+      'filter' => [ 'PHONE' =>  $contact['PHONE']],
+      'select' => [ 'ID'],
+    ]);
+    return $list;
+  }
+
+ ?>
